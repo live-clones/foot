anion-examio/foot-mirror

Add support for background blur
This patch adds a new config option: colors{,2}.blur=no|yes. When
enabled, transparent background are also blurred.

Note that this requires the brand new ext-background-effect-v1
protocol, and specifically, that the compositor implements the blur
effect.

terminal.c
@@ -4786,3 +4786,11 @@ term_theme_toggle(struct terminal *term)
     term_damage_margins(term);
     render_refresh(term);
 }
+
+const struct color_theme *
+term_theme_get(const struct terminal *term)
+{
+    return term->colors.active_theme == COLOR_THEME1
+        ? &term->conf->colors
+        : &term->conf->colors2;
+}

live-clones/foot

terminal.c
@@ -4809,3 +4809,11 @@ term_theme_toggle(struct terminal *term)
     term_damage_margins(term);
     render_refresh(term);
 }
+
+const struct color_theme *
+term_theme_get(const struct terminal *term)
+{
+    return term->colors.active_theme == COLOR_THEME_DARK
+        ? &term->conf->colors_dark
+        : &term->conf->colors_light;
+}